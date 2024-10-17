357a1bd6535f8d2c616a19eea63189872e96e0f5
william <hr.g5studio@gmail.com>
2024-10-17 13:48:53 +0800

    chore: add layout context

--- src/utilities/config/global.d.ts
+++ src/utilities/config/global.d.ts
@@ -1,8 +1,11 @@
 type CheckFn = () => boolean;
+type PureFun = () => void;
 type ArrowFn<Arg, ReturnValue = void> = (arg: Arg) => ReturnValue;
 type MultipleArgsArrowFn<Args extends Array, ReturnValue = void> = (...args: Args) => ReturnValue;
 type NavigatorTheme = Record<'memberCenter' | 'login' | 'register' | 'default', string>;
 type PartialRecord<K, T> = Partial<Record<K, T>>;
+type DomSize = Pick<DOMRect, 'height' | 'width' | 'top' | 'left'>;
+type Nullable<T> = T | null | undefined;
 
 declare interface Window {
   gtag?: MultipleArgsArrowFn<unknown[], void>;
---
+++ src/utilities/context/layout-context.tsx
@@ -0,0 +1,125 @@
+import { SystemTheme } from '@shared/enums';
+import { IBaseComponentProps } from '@shared/interfaces';
+import { DomPropertyCbParams } from '@utilities/directives/dom-property-directive';
+import { Accessor, Setter, createContext, createSignal, useContext } from 'solid-js';
+
+export type LayoutScrollRef = HTMLElement | Window | undefined;
+
+export type LayoutSizeDomPropertyCbParams = DomPropertyCbParams<
+  ['domRectWidth', 'domRectHeight', 'domRectTop', 'domRectLeft']
+>;
+
+export type Theme = Extract<SystemTheme, SystemTheme.Dark | SystemTheme.Light>;
+
+export type LayoutContextValue = {
+  /**
+   * 頁面實際內容之外的header高度
+   * @description 體育內頁的header是存在實際內容中，並非真正的header
+   */
+  headerAreaHeight: Accessor<number>;
+  /**
+   * 主要滾動軸
+   */
+  mainScrollRef: Accessor<LayoutScrollRef>;
+  /**
+   * 頁面實際內容
+   */
+  contentLayoutSize: Accessor<DomSize>;
+  contentLayoutRef: Accessor<HTMLElement | undefined>;
+  /**
+   * 頁面可視區域
+   */
+  containerSize: Accessor<DomSize>;
+  /**
+   * 取得當前系統風格資訊
+   */
+  theme: Accessor<Theme>;
+  /**
+   * 主要滾動軸區域
+   * @description 頁面實際內容 + 賽事分類快速切換選單 + 熱門賽事服務
+   */
+  mainContentAreaSize: Accessor<DomSize>;
+};
+
+export type LayoutContextSetter = {
+  setHeaderAreaHeight: Setter<number>;
+  setMainScrollRef: Setter<LayoutScrollRef>;
+  setContentLayoutSize: Setter<DomSize>;
+  setContainerSize: Setter<DomSize>;
+  setContentLayoutRef: Setter<HTMLElement | undefined>;
+  setTheme: Setter<Theme>;
+  setMainContentAreaSize: Setter<DomSize>;
+};
+
+type ContextType = [LayoutContextValue, LayoutContextSetter];
+
+const LoginContext = createContext<ContextType>();
+
+const useContentLayout = (): Pick<LayoutContextSetter, 'setContentLayoutRef' | 'setContentLayoutSize'> &
+  Pick<LayoutContextValue, 'contentLayoutRef' | 'contentLayoutSize'> => {
+  const [contentLayoutSize, setContentLayoutSize] = createSignal<DomSize>({
+    height: 0,
+    width: 0,
+    top: 0,
+    left: 0,
+  });
+  const [contentLayoutRef, setContentLayoutRef] = createSignal<HTMLElement>();
+
+  return {
+    contentLayoutSize,
+    setContentLayoutSize,
+    contentLayoutRef,
+    setContentLayoutRef,
+  };
+};
+
+export const LayoutProvider = (props: IBaseComponentProps) => {
+  const [theme, setTheme] = createSignal<Theme>(SystemTheme.Light);
+  const [headerAreaHeight, setHeaderAreaHeight] = createSignal(0);
+  const [mainScrollRef, setMainScrollRef] = createSignal<LayoutScrollRef>();
+  const [containerSize, setContainerSize] = createSignal<DomSize>({
+    height: 0,
+    width: 0,
+    top: 0,
+    left: 0,
+  });
+  const [mainContentAreaSize, setMainContentAreaSize] = createSignal<DomSize>({
+    height: 0,
+    width: 0,
+    top: 0,
+    left: 0,
+  });
+
+  const { contentLayoutSize, setContentLayoutSize, contentLayoutRef, setContentLayoutRef } = useContentLayout();
+
+  const context: ContextType = [
+    {
+      headerAreaHeight,
+      mainScrollRef,
+      containerSize,
+      contentLayoutSize,
+      contentLayoutRef,
+      theme,
+      mainContentAreaSize,
+    },
+    {
+      setHeaderAreaHeight,
+      setMainScrollRef,
+      setContentLayoutSize,
+      setContainerSize,
+      setContentLayoutRef,
+      setTheme,
+      setMainContentAreaSize,
+    },
+  ];
+
+  return <LoginContext.Provider value={context}>{props.children}</LoginContext.Provider>;
+};
+
+export function useLayoutContext() {
+  const context = useContext(LoginContext);
+  if (!context) {
+    throw new Error('useLayoutContext must be used within a LoginContext');
+  }
+  return context;
+}
